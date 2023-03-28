2a5743d1cc965f08714efee167cef843f1cba7e1
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-28 10:37:46 +0200

    fixed challenge 3 code

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA2/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA2/scripts.js
@@ -19,3 +19,17 @@ console.log(user, surname);
 const date = '10/07/2014'
 
 console.log(date);
+
+
+/* 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn('Security scan starting');
+
+/* It is important to let user know when they can close the page  */
+
+console.info('Please wait for scan to complete before closing the browser.');
