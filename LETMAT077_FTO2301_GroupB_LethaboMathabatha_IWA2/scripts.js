4987c3aaeab7166dfab9fbee805c5942753d7b9f
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-28 10:20:14 +0200

    fixed challenge 2 code

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA2/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA2/scripts.js
@@ -9,3 +9,13 @@ const surname = 'Smith'
 
 
 console.log(user, surname);
+
+
+/** (c) ACME Inc. 2010 */
+
+/** 
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date);
