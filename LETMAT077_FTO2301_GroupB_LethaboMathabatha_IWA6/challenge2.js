8080c37bfec53d6e6f65316305d2875c02422da6
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-31 17:12:57 +0200

    fixed sub 6 challenge2 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA6/challenge2.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA6/challenge2.js
@@ -5,15 +5,24 @@ const tax = '12%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
+if (hourOfDay === 0 && minuteOfDay === 0) {
+	const taxAsDecimal = parseInt(tax) / 100;
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent
+    console.log('R'+ balance.toFixed(2))
+}   
+    else if (hourOfDay === undefined && minuteOfDay === undefined) {
+        console.log('Balance cannot be calculated. Time Unknown')
+    }
 	
-console.log(balance.toFixed(3))
+    else ( hourOfDay === undefined || minuteOfDay === undefined); {
+        console.log('Balance cannot be calculated. Time Unknown')
+    }
+
+
+
