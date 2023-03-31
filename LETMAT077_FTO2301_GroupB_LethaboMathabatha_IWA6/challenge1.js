30089af133e48f27b31b97c305a479ae9df7a2e9
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-03-31 16:19:38 +0200

    fixed sub 6 challenge1 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA6/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA6/challenge1.js
@@ -1,12 +1,11 @@
-
-
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const primaryValid = !isNaN(primaryPhone) && primaryPhone.length === 10; 
+const secondaryValid = !isNaN(secondaryPhone) && secondaryPhone.length === 10;
 
-console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid);
+
