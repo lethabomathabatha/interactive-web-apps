ed33f55010b8610135fa5d69c3442857a3ba9ce3
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-03 15:08:03 +0200

    fixed sub 7 challenge1 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA7/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA7/challenge1.js
@@ -4,3 +4,11 @@
 
 const value = 3
 console.log(value + 4 + value)
+
+// in the initial code, 343 was logged because 3 was a string value, and treated as such. 
+// The result of this was concactenation, instead of mathematical evaluation
+// removing the " " turned the 3 into a number
+
+// alternative solution
+const value1 = 3
+console.log(parseInt(value1) + 4 + parseInt(value1))
