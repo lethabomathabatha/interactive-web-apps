a87e11f88d58658fb812a26ada22ab03aa1f4e53
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-03 15:01:46 +0200

    fixed sub 7 challenge1 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA7/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA7/challenge1.js
@@ -1,5 +1,6 @@
 // The following code is meant to add the following three values together and log 10.
-// However, it currently logs 343 instead. Please correct the mistake, and explain why 343 was logged initially:
+// However, it currently logs 343 instead. Please correct the mistake, and 
+// explain why 343 was logged initially:
 
-const value = "3"
+const value = 3
 console.log(value + 4 + value)
