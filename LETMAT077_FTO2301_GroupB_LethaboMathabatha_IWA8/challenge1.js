afe031541224adad4f84a55fb9330da3f5ef3062
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-04 10:00:16 +0200

    fixed sub 8 challenge1 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA8/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA8/challenge1.js
@@ -20,7 +20,7 @@ const leoBalance = '-10'
 
 const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
+// const sarahBalance = '-4582.21000111'
 const sarahNumber = '13'
 const sarahStreet = 'William Close'
 const sarahPostal = '0310'
@@ -28,28 +28,32 @@ const sarahBalance = '-5'
 
 // Only change below this line
 
-const leo {
-	name = leoName + leoSurname
-	balance = leoBalance
-	access id = 47afb389-8014-4d0b-aff3-e40203d2107f
-	age = 24
-	address {
-		number = leoNumber
-		street = leoStreet
-		postal-code = leoPostal
+const leo = {
+	debtorName: leoName,
+	balance: leoBalance * -1,
+	accessId: "47afb389-8014-4d0b-aff3-e40203d2107f",
+	age: 24,
+	address: {
+		number: leoNumber,
+		street: leoStreet,
+		postalCode: leoPostal,
 	}
 }
 
-const sarah {
-	name = saraName + saraSurname
-	age = 62
-	access id = 6b279ae5-5657-4240-80e9-23f6b635f7a8
-	balance = sarahBalance
-	address {
-		number = sarahNumber
-		street = sarahStreet
-		postal-code = sarahPostal
+const sarah = {
+	debtorName: sarahName + sarahSurname,
+	balance: sarahBalance * -1,
+	age: 62,
+	accessId: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
+	address: {
+		number: sarahNumber,
+		street: sarahStreet,
+		postalCode: sarahPostal,
 	}
+}
 
-console.log(leo, leo[address][postal-code])
-console.log(sarah, sarah[address][postal-code])
+console.log(leo.address.postalCode)
+console.log(sarah.address.postalCode)
+console.log(sarah.balance)
+
+
