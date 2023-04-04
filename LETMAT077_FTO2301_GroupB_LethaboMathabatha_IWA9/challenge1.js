928bc88de946b077eea330ce2cfa6a604da6bfc3
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-04 15:49:39 +0200

    added midnight calc conditions js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA9/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA9/challenge1.js
@@ -37,33 +37,35 @@ const minuteOfDay = 0;
 const expenses = {
     food: 51.7501,
     transport:  10.2,
-}
-  
-const tax = {
-    734: '3%',
-    234: '20%',
-    913: '12%',
-    415: '38%',
-    502: '42%',
-}
 
-const rent = {
-    none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
+  	tax: {
+		734: '3%',
+		234: '20%',
+		913: '12%',
+		415: '38%',
+		502: '42%',
+	},
+
+	rent: {
+		none: 0,
+		'small-room': 200,
+		'large-room': 300,
+		'small-apartment': 400,
+		'large-apartment': 800,
+		'small-house': 1200,
+		'large-house': 2400,
+	},
 }
 
 // You can change below however you want
-
-const taxAsDecimal = parseInt(tax['913']) / 100;
+if (hourOfDay === 0 && minuteOfDay === 0) {
+const taxAsDecimal = parseInt(expenses.tax[913]) / 100;
 const startingAfterTax = salary  * (1 - taxAsDecimal);
-const rentAmount = parseInt(rent["large-apartment"]);
+const rentAmount = parseInt(expenses.rent["large-apartment"]);
 const taxableAmount = startingAfterTax - rentAmount;
 
 const balance = taxableAmount - expenses.transport - expenses.food;
 console.log(balance.toFixed(2));
-
+} else {
+	console.log("Balance cannot be calculated at this time.")
+}
