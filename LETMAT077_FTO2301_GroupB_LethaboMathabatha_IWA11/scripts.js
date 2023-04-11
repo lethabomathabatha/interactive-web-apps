e3418e684b94e1c4719eb070eeecb0d3a86fe074
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-11 13:15:18 +0200

    selected DOM elements

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA11/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA11/scripts.js
@@ -15,45 +15,24 @@ since the HTML is created by the server that reads from a database. This also me
 need to rely on ID and class attributes to select specific HTML nodes.
 */
 
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
-
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
-
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
-
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
-
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+// Order 1
+const order1 = document.querySelector('[data-key="order1"]');
+const biscuits1 = order1.querySelector('.biscuits.count');
+const donuts1 = order1.querySelector('.donuts.count');
+const pancakes1 = order1.querySelector('.pancakes.count');
 
 
-document(biscuits.p-count).pending = 3-status ? 1-biscuits + 2-biscuits + 3-biscuits: 0
-document(biscuits.p-count).delivered 3-status ? 0 : 1-biscuits + 2-biscuits + 3-biscuits
+// Order 2
+const order2 = document.querySelector('[data-key="order2"]');
+const biscuits2 = order2.querySelector('.biscuits.count');
+const donuts2 = order2.querySelector('.donuts.count');
+const pancakes2 = order2.querySelector('.pancakes.count');
 
-document(donuts.p-count).pending = 3-status ? 1-donuts + 2-donuts + 3-donuts: 0
-document(donuts.p-count).delivered 3-status ? 0 : 1-donuts + 2-donuts + 3-donuts
+// Order 3
+const order3 = document.querySelector('[data-key="order3"]');
+const biscuits3 = order3.querySelector('.biscuits.count');
+const donuts3 = order3.querySelector('.donuts.count');
+const pancakes3 = order3.querySelector('.pancakes.count');
 
-document(pancakes.p-count).pending = 3-status ? 1-pancakes+ 2-pancakes+ 3-pancakes: 0
-document(pancakes.p-count).delivered 3-status ? 0 : 1-pancakes+ 2-pancakes + 3-pancakes
+// Delivery status
