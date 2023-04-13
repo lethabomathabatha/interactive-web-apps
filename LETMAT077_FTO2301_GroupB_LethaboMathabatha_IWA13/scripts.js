8b7348bdddb4c78b2adfa90e89e228e5f10a4891
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-13 14:59:42 +0200

    resolved sub13 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA13/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA13/scripts.js
@@ -22,20 +22,20 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const logCalc = () => { 
+    const isString = typeof calculated === 'string'
+    const calculatedAsNumber = isString ? parseInt(calculated) : calculated
+    calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
+const calcUser = () => {
+  logCalc ()
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
@@ -57,3 +57,12 @@ calcUser()
 
 checkUser()
 calcUser()
+
+// incorrect syntax when declaring variables. " = " was used instead of just a blank space
+// no " = " after declaring variable, and before function
+// the expression for const isString is incorrectly written as "typeof calculated = 'numerical-string'" 
+// instead of "typeof calculated === 'string'", which checks if this expression is true or false
+// "calculated" should go through parseInt for it to further in the code because, as proven in the line above,
+// it is a string, and not a number
+// in the calculation, the strict equality (===) is incorrectly used as it is not supposed to compare the values/variables,
+// but rather calculate (and assign " calculatedAsNumber + 1 "). Therefore, the "=" operator is more appropriate.
