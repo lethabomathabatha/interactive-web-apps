2d87fb4fff6aaac89f8532cc512313511ad36b1c
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-14 16:02:57 +0200

    added comment to sub 14 chall2 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA14/challenge2.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA14/challenge2.js
@@ -16,42 +16,47 @@ Please fix the code above the "// Not allowed to change below this" comment, whi
 
 // script.js
 
-function add = a, b => { a + b }
+const add = (a, b) => { 
+  return a + b 
+}
 
-function multiply = a, b => { a - b }
+const multiply  = (a, b) => { 
+  return a * b 
+}
 
-function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
+function internal () {
+ const added = add(this.internal.a, this.internal.b)
+ const multiplied = multiply(this.internal.a, this.internal.b)
+   console.log(added * multiplied)
 }
 
 // Not allowed to change below this
 
 const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
+ internal: {
+   a: 2,
+   b: 4,
+   c: 8,
+ },
+ add,
+ multiply,
+ calculate: internal
 }
 
 const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
+ internal: {
+   a: 2,
+   b: 2,
+   c: 3,
+ },
+ add,
+ multiply,
+ calculate: internal
 }
 
 example1.calculate()
 example2.calculate()
+
 
 // the syntax for the function declarations is incorrect.
 // 'this' is possibly meing misused because it is not accessing 'add' from within the object
