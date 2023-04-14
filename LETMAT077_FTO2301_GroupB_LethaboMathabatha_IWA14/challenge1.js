a4b003e2a3f9e427ea5f49f8febf865817f23c64
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-14 14:14:33 +0200

    added sub 14 challenge1 js

---
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA14/challenge1.js
@@ -0,0 +1,27 @@
+/*
+The following code should take the values provided as variables, and create logTwice function. 
+The logTwice function should take a provided parameter and log it two times. In other words, 
+the following should be logged in the console twice:
+
+Hello, John (35). I love coding!
+
+ 
+Unfortunately at the moment went attempting to run the code the following error is returned:
+
+hobby is not a function
+*/
+
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+}
+
+hobby()
