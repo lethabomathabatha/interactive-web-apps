35e355be2db9cf42b5b1af5bce0ad741d377aeef
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-04-17 15:29:51 +0200

    resolved sub15 js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA15/challenge1.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA15/challenge1.js
@@ -43,23 +43,49 @@ const data = {
 
 // Only edit below
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+// accessing all arrays within the 'lists' object
+const first = data.lists[0][1]
+const second = data.lists[1][1]
+const third = data.lists[2][1]
+
+console.log(data.lists[0][1])
+console.log(data.lists[1][1])
+console.log(data.lists[2][1])
+
+
+// accessing all the last values from the arrays within the 'lists' object
+const firstLast = first[first.length - 1]; 
+const secondLast = second[second.length - 1]; 
+
+const thirdLast = third[third.length - 1]; 
+
+
+// comparing the values within the arrays with each other
 
 const result = []
 
 const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
+    const firstLast = data.lists[0][1][data.lists[0][1].length - 1];
+    const secondLast = data.lists[1][1][data.lists[1][1].length - 1];
+    const thirdLast = data.lists[2][1][data.lists[2][1].length - 1];
 
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
+    let biggestIndex = -1;
+    if (firstLast > secondLast && firstLast > thirdLast) {
+        biggestIndex = 0;
+    } else if (secondLast > firstLast && secondLast > thirdLast) {
+        biggestIndex = 1;
+    } else {
+        biggestIndex = 1;
+    }
+
+    const biggest = data.lists[biggestIndex][1][data.lists[biggestIndex][1].length - 1];
+    data.lists[biggestIndex][1].pop();
+
+    return biggest;
 }
+
+
+
 
 // Only edit above
 
