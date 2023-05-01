93c01ae338f84072de2d432b973c90c10b072d07
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-01 14:25:07 +0200

    edited sub18 scripts.js

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA18/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_IWA18/scripts.js
@@ -1,59 +1,117 @@
-/**
- * A handler that fires when a user drags over any element inside a column. In
- * order to determine which column the user is dragging over the entire event
- * bubble path is checked with `event.path` (or `event.composedPath()` for
- * browsers that don't support `event.path`). The bubbling path is looped over
- * until an element with a `data-area` attribute is found. Once found both the
- * active dragging column is set in the `state` object in "data.js" and the HTML
- * is updated to reflect the new column.
- *
- * @param {Event} event 
- */
-const handleDragOver = (event) => {
-    event.preventDefault();
-    const path = event.path || event.composedPath()
-    let column = null
+// script.js
+import { COLUMNS } from './data.js'
+import { TABLES } from './data.js';
+import { state } from './data.js';
+// import { createOrderData } from './data.js';
+// import { updateDragging } from './data.js';
 
-    for (const element of path) {
-        const { area } = element.dataset
-        if (area) {
-            column = area
-            break;
-        }
-    }
+// import { createOrderHtml } from './view.js';
+// import { html } from './view.js';
+// import { updateDraggingHtml } from './view.js';
+import { moveToColumn } from './view.js';
 
-    if (!column) return
-    updateDragging({ over: column })
-    updateDraggingHtml({ over: column })
-}
+import { createOrderData } from "./data.js";
+import { updateDragging } from "./data.js";
+import { createOrderHtml } from "./view.js";
+import { html } from "./view.js";
+import { updateDraggingHtml } from "./view.js";
 
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {}
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
-const handleEditSubmit = (event) => {}
-const handleDelete = (event) => {}
+// User story 1: Add focus on the "Add Order" button when the page loads
+const addButton = document.querySelector('[data-add]');
+addButton.focus();
 
-html.add.cancel.addEventListener('click', handleAddToggle)
-html.other.add.addEventListener('click', handleAddToggle)
-html.add.form.addEventListener('submit', handleAddSubmit)
+// User story 2: Add an event listener to the "?" icon to toggle the "Help" overlay visibility
+const helpButton = document.querySelector('[data-help]');
+const helpOverlay = document.querySelector('[data-help-overlay]');
+helpButton.addEventListener('click', () => {
+  helpOverlay.showModal();
+});
 
-html.other.grid.addEventListener('click', handleEditToggle)
-html.edit.cancel.addEventListener('click', handleEditToggle)
-html.edit.form.addEventListener('submit', handleEditSubmit)
-html.edit.delete.addEventListener('click', handleDelete)
+// User story 3: Add an event listener to the "Close" button of the "Help" overlay 
+const helpCloseButton = document.querySelector('[data-help-cancel]');
+helpCloseButton.addEventListener('click', () => {
+  helpOverlay.close();
+});
 
-html.help.cancel.addEventListener('click', handleHelpToggle)
-html.other.help.addEventListener('click', handleHelpToggle)
+// User story 4: Add an event listener to the "Close" button of all overlays to close the overlay and return focus to the "Add Order" button
+const overlayCloseButtons = document.querySelectorAll('[data-add-cancel],[data-edit-cancel], [data-help-cancel]');
+overlayCloseButtons.forEach(closeButton => {
+  closeButton.addEventListener('click', () => {
+    closeButton.closest('.overlay').close();
+    addButton.focus();
+  });
+});
 
-for (const htmlColumn of Object.values(html.columns)) {
-    htmlColumn.addEventListener('dragstart', handleDragStart)
-    htmlColumn.addEventListener('dragend', handleDragEnd)
-}
+// User story 5: Add an event listener to the "Add Order" button to show the "Add Order" overlay and move focus to the "Item" input field
+const addOrderButton = document.querySelector('[data-add]');
+const addOrderOverlay = document.querySelector('[data-add-overlay]');
+const addOrderTitle = document.querySelector('[data-add-title]');
+addOrderButton.addEventListener('click', () => {
+  addOrderOverlay.showModal();
+  addOrderTitle.focus();
+});
 
-for (const htmlArea of Object.values(html.area)) {
-    htmlArea.addEventListener('dragover', handleDragOver)
-}
+// User story 6: Add an event listener to the "Cancel" button of the "Add Order" overlay to close the overlay and return focus to the "Add Order" button, without adding a new order
+const addOrderCancelButton = document.querySelector('[data-add-cancel]');
+addOrderCancelButton.addEventListener('click', () => {
+  addOrderOverlay.close();
+  addButton.focus();
+});
+
+// User story 7: Add an event listener to the "Add" button of the "Add Order" overlay to close the overlay, add a new order to the "Ordered" column, and return focus to the "Add Order" button
+const addOrderForm = document.querySelector('[data-add-form]');
+addOrderForm.addEventListener('submit', event => {
+  event.preventDefault(); 
+  const orderTitle = addOrderForm.elements.title.value.trim();
+  const orderTable = addOrderForm.elements.table.value;
+  const orderedColumn = document.querySelector('[data-column="ordered"]');
+  const newOrderElement = createNewOrderElement(orderTitle, orderTable);
+  orderedColumn.appendChild(newOrderElement);
+  addOrderOverlay.close();
+  addButton.focus();
+  addOrderForm.reset();
+});
+
+function createNewOrderElement(title, table) {
+    const newOrderElement = document.createElement('div');
+    newOrderElement.classList.add('order');
+    newOrderElement.innerHTML = `
+      <div class="order__title">${title}</div>
+      <div class="order__table">Table ${table}</div>
+    `;
+    return newOrderElement;
+  }
+
+  // User story 8: If the “Add Order” overlay is closed (either with “Cancel” or “Add”) and it is opened again it should be blank (not have information from the last time it was opened).
+  addOrderOverlay.addEventListener('close', () => {
+    addOrderForm.reset();
+  });
+
+  // User story 9: If an order has been added and it is clicked on the “Edit Order” overlay should appear and the focus should be on the “Item” input field.
+  const orders = document.querySelectorAll('[data-column="ordered"]');
+  orders.forEach(order => {
+    order.addEventListener('click', () => {
+      const editOrderOverlay = document.querySelector('[data-edit-overlay]');
+      const editOrderTitle = document.querySelector('[data-edit-title]');
+      editOrderOverlay.showModal();
+      editOrderTitle.focus();
+    });
+  });
+
+  // User story 10: If the “Delete” button is pressed in the Edit Order overlay the overlay should be closed and the order should be removed entirely.
+  const editOrderDeleteButton = document.querySelector('[data-edit-delete]');
+  editOrderDeleteButton.addEventListener('click', () => {
+    const editOrderOverlay = document.querySelector('[data-edit-overlay]');
+    editOrderOverlay.close();
+    const order = document.querySelector('.order');
+    order.remove();
+  });
+
+
+// Helper function to create
+
+
+
+
+
